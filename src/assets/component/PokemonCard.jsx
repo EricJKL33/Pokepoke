8bf2824381aeb5a768226ca864ed26b1fa30c386
EricJKL33 <eric.jkl@icloud.com>
2023-03-29 09:47:45 +0200

    newComp

--- src/assets/component/PokemonCard.jsx
+++ src/assets/component/PokemonCard.jsx
@@ -1,14 +1,83 @@
+// function PokemonCard() {
+//     const pokemon = pokemonList[0];
+//   return (
+//     <figure className="card">
+//       <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img" />
+//       <figcaption>{pokemon.name}</figcaption>
+//     </figure>
+//   );
+// }
+
+// function PokemonCard() {
+//   const [pokemon, setPokemon] = useState(pokemonList[0]);
+
+//   const handleButtonClick = () => {
+//     setPokemon(pokemonList[1]);
+//   };
+
+//   return (
+//     <figure className="card">
+//       {pokemon.imgSrc ? (
+//         <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img" />
+//       ) : (
+//         <p>???</p>
+//       )}
+//       <figcaption>{pokemon.name}</figcaption>
+//       <button onClick={handleButtonClick}>Next Pokemon</button>
+//     </figure>
+//   );
+// }
+
+
+
+
+
+// const pokemonList = [
+//     {
+//         name: "bulbasaur",
+//         imgSrc:
+//         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+//     },
+//     {
+//         name: "mew",
+//     },
+// ];
+
+// export default PokemonCard;
+
+
+
+import { useState } from "react";
+
 function PokemonCard() {
+  const [pokemon, setPokemon] = useState(pokemonList[0]);
+
+  const handleButtonClick = () => {
+    setPokemon(pokemonList[1]);
+  };
+
   return (
     <figure className="card">
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-        className="card-img"
-      />
-      <figcaption>bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img" />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+      <button onClick={handleButtonClick}>Next Pokemon</button>
     </figure>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
